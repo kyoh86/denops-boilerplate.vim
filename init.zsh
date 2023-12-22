bfe9f5dd51327950798ab833478391a503da6c4c
kyoh86 <me@kyoh86.dev>
2023-12-23 03:53:57 +0900

    first commit

---
+++ init.zsh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# ファイル名の中のboilerplateを任意の文字列に置換したものにmvする
+# ファイルの中のboilerplateを任意の文字列に置換したものにsedする
+# 例: ./init.zsh hogehoge
+# 置換後の文字列が指定されない場合はエラー
+NAME="$1"
+if [ -z "${NAME}" ]; then
+  echo "NAME is not set";
+  exit 1;
+fi
+
+find ./* -type f -not -name 'init.zsh' | while read -r file; do
+  sed -i '' -e "s/boilerplate/${NAME}/g" "${file}";
+done
+
+find ./* -name '*boilerplate*' | while read -r file; do
+  mv "$file" "${file//boilerplate/${NAME}}";
+done
