97047a2aa2c4b1d2ba9fc1b38be8a9cd074d0eb0
kyoh86 <me@kyoh86.dev>
2024-01-06 09:04:39 +0900

    Update dependencies

--- denops/boilerplate/main.ts
+++ denops/boilerplate/main.ts
@@ -1,14 +1,14 @@
 import { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
 import {} from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
-import { join } from "https://deno.land/std@0.210.0/path/mod.ts";
-import { ensureFile } from "https://deno.land/std@0.210.0/fs/mod.ts";
+import { join } from "https://deno.land/std@0.211.0/path/mod.ts";
+import { ensureFile } from "https://deno.land/std@0.211.0/fs/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.11.0/mod.ts";
 import {
   getLogger,
   handlers,
   setup,
-} from "https://deno.land/std@0.210.0/log/mod.ts";
+} from "https://deno.land/std@0.211.0/log/mod.ts";
 
 export async function main(denops: Denops) {
   const cacheFile = join(xdg.cache(), "denops-boilerplate-vim", "log.txt");
