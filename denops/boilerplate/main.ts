8c1eae459d5e337a14230935e0851b4df863c9c6
kyoh86 <me@kyoh86.dev>
2024-01-21 05:08:18 +0900

    fix some lints

--- denops/boilerplate/main.ts
+++ denops/boilerplate/main.ts
@@ -5,8 +5,9 @@ import { join } from "https://deno.land/std@0.212.0/path/mod.ts";
 import { ensureFile } from "https://deno.land/std@0.212.0/fs/mod.ts";
 import { ensure, is } from "https://deno.land/x/unknownutil@v3.14.1/mod.ts";
 import {
+  ConsoleHandler,
   getLogger,
-  handlers,
+  RotatingFileHandler,
   setup,
 } from "https://deno.land/std@0.212.0/log/mod.ts";
 
@@ -16,8 +17,8 @@ export async function main(denops: Denops) {
 
   setup({
     handlers: {
-      console: new handlers.ConsoleHandler("DEBUG"),
-      cache: new handlers.RotatingFileHandler("DEBUG", {
+      console: new ConsoleHandler("DEBUG"),
+      cache: new RotatingFileHandler("DEBUG", {
         filename: cacheFile,
         maxBytes: 1024 * 1024,
         maxBackupCount: 1,
