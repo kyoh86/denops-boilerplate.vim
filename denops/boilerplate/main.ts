b0b1987c4c1487b5055d758705cd7c59c1636ff2
kyoh86 <me@kyoh86.dev>
2024-01-21 20:01:44 +0900

    Update dependencies

--- denops/boilerplate/main.ts
+++ denops/boilerplate/main.ts
@@ -1,5 +1,5 @@
-import { Denops } from "https://deno.land/x/denops_std@v5.2.0/mod.ts";
-import {} from "https://deno.land/x/denops_std@v5.2.0/helper/mod.ts";
+import { Denops } from "https://deno.land/x/denops_std@v5.3.0/mod.ts";
+import {} from "https://deno.land/x/denops_std@v5.3.0/helper/mod.ts";
 import xdg from "https://deno.land/x/xdg@v10.6.0/src/mod.deno.ts";
 import { join } from "https://deno.land/std@0.212.0/path/mod.ts";
 import { ensureFile } from "https://deno.land/std@0.212.0/fs/mod.ts";
