5f5171013dd190562f3d1d2566861618ab886e4a
kyoh86 <me@kyoh86.dev>
2024-08-01 10:16:15 +0900

    Update dependencies

--- denops/boilerplate/main.ts
+++ denops/boilerplate/main.ts
@@ -1,4 +1,4 @@
-import type { Denops } from "jsr:@denops/std@7.0.0";
+import type { Denops } from "jsr:@denops/std@7.0.1";
 import { ensure, is } from "jsr:@core/unknownutil@3.18.1";
 
 export function main(denops: Denops) {
